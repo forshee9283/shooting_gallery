8ac19eccb82e79724318b34fe9adb04c5c616db6
Jeff Forshée <jeffrey.forshee@gmail.com>
2024-07-15 19:12:08 -0500

    Added some notes on the hardware side and did significant work on the software side. Communication is up and LEDs are LEDing.

---
+++ software/scoreboard/source.c
@@ -0,0 +1,271 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "pico/stdlib.h"
+#include "pico/sem.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+#include "hardware/irq.h"
+#include "hardware/spi.h"
+
+#define LED_PIN PICO_DEFAULT_LED_PIN
+#define TEST_LED_0 2
+#define SW_1 28
+#define SW_2 27
+#define SW_3 26
+
+#define SS_SPI spi0
+#define SS_SPI_RATE 1000000 //1MHz
+#define SS_CLK 6
+#define SS_D 7
+#define SS_LAT 8
+#define SS_BLANK 9
+
+#define BAUD_RATE 31250 //MIDI standard data rate
+#define UART0_TX 0
+#define UART0_RX 1
+#define UART1_TX 4
+#define UART1_RX 5
+
+#define MAX_PLAYERS 4 //Set for number of player scoreboards in the chain
+#define DISPLAY_UNITS MAX_PLAYERS + 1 //Total number of displays with clock
+volatile int score[MAX_PLAYERS] = {0};
+
+volatile bool tiger_1;
+volatile int led_on_timer =0;
+volatile int piezo_trig_cnt =0;
+volatile uint game_time=0;
+
+uint8_t segments[10] = {
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111  // 9
+    };
+uint8_t boot_message[24] = {
+    0b00111001, //C
+    0b00111111, //O
+    0b00111111, //O
+    0b01110011, //P
+    0b01111001, //E
+    0b01010000, //r
+    0b00000010, //'
+    0b01101101, //S
+    0b00000000, //space
+    0b01101101, //S
+    0b00111110, //U
+    0b01110011, //P
+    0b01111001, //E
+    0b01010000, //r
+    0b00000000, //space
+    0b01111100, //b
+    0b00111110, //U
+    0b00111000, //L
+    0b00111000, //L
+    0b01101101, //S
+    0b01111001, //E
+    0b01101110, //y
+    0b01111001, //E
+    0b00000000  //space
+    };
+
+
+int ss_int_write(int num){
+    uint8_t data_to_send [4];
+    data_to_send [3] = segments[num % 10];
+    data_to_send [2] = segments[(num /10) % 10];
+    data_to_send [1] = segments[(num /100) % 10];
+    data_to_send [0] = segments[(num /1000) % 10];
+    spi_write_blocking(SS_SPI, &data_to_send[0], 4); // Send one byte
+}
+int ss_time_write(int num){
+    uint8_t data_to_send [4];
+    uint8_t blink = ((num % 10)>4) ? 0x00 : 0x80; //blink ":" between min and sec
+    int seconds = (num/10)%60;
+    data_to_send [3] = segments[num % 10];
+    data_to_send [2] = segments[(seconds) % 10] | 0x80;
+    data_to_send [1] = segments[(seconds /10) % 10]| blink;
+    data_to_send [0] = segments[(num /600) % 10] | blink;
+    spi_write_blocking(SS_SPI, &data_to_send[0], 4); // Send one byte
+}
+
+bool timer_callback (struct repeating_timer *t) {
+    gpio_put(SS_LAT, 0);
+    if (game_time != 0)
+    {
+        game_time--;
+    }
+    ss_time_write(game_time);
+    for (size_t i = 0; i < MAX_PLAYERS; i++)
+        {
+            ss_int_write(score[i]);
+        }
+    gpio_put(SS_LAT, 1);
+    return true;
+}
+
+// void button_callback(){
+// }
+
+
+void set_tlc5916_current(uint8_t* currents, int groups) {
+    int total_chips = groups * 4;
+
+    int blank_start_special [5] = {1,0,1,1,1};
+    int latch_start_special [5] = {0,0,0,1,0};
+    int blank_end_special [5] = {1,0,1,1,1};
+    int latch_end_special [5] = {0,0,0,0,0};
+    
+    // Enter special mode
+    spi_deinit(SS_SPI);
+    gpio_set_function(SS_D, GPIO_FUNC_SIO);
+    gpio_set_dir(SS_D, true);
+    gpio_set_function(SS_CLK, GPIO_FUNC_SIO);
+    gpio_set_dir(SS_CLK, true);
+    for (int i = 0; i < 4; i++) {
+    gpio_put(SS_BLANK, blank_start_special[i]);
+    gpio_put(SS_LAT, latch_start_special[i]);
+    gpio_put(SS_CLK,1);
+    sleep_us(100);
+    gpio_put(SS_CLK,0);
+    sleep_us(100);
+    }
+
+    // // Set the output current for each group
+    for (int g = 0; g < groups; g++) {
+        uint8_t current = currents[g];
+        for (int i = 0; i < 4; i++) {
+            for (int bit = 0; bit < 8; bit++) {
+                uint8_t current_bit = (current >> (7 - bit)) & 0x01; // Extract the bit
+                gpio_put(SS_LAT,(bit==7)?1:0);
+                gpio_put(SS_D, current_bit); // Send the bit
+                gpio_put(SS_CLK,1);
+                sleep_us(100);
+                gpio_put(SS_CLK,0);
+                sleep_us(100);
+            }
+        }
+    }
+
+    // Return to normal mode
+    for (int i = 0; i < 4; i++) {
+    gpio_put(SS_BLANK, blank_end_special[i]);
+    gpio_put(SS_LAT, latch_end_special[i]);
+    gpio_put(SS_CLK,1);
+    sleep_us(100);
+    gpio_put(SS_CLK,0);
+    sleep_us(100);
+    }
+    spi_init(SS_SPI, SS_SPI_RATE); // Initialize SPI
+    gpio_set_function(SS_D, GPIO_FUNC_SPI);
+    gpio_set_function(SS_CLK, GPIO_FUNC_SPI);
+    gpio_put(SS_BLANK, 0);
+}
+//Currently hardcoded for 4 displays
+void boot_text() {
+    
+    for (size_t i = 0; i < 100; i++)
+    {
+        gpio_put(SS_LAT, 0);
+        for (size_t j = 0; j < 12; j++) //This will need redoing with all the displays
+        {
+            spi_write_blocking(SS_SPI, &boot_message[((i + j) % sizeof(boot_message))], 1);
+        }
+        gpio_put(SS_LAT, 1);
+        sleep_ms(400);
+    }
+    
+
+}
+
+//Com Functions - These are based on midi
+void target_on(uint8_t channel, uint8_t note, u_int8_t data){
+            uart_putc(uart0, 0x90 | channel); //Must be 0 - 15
+            uart_putc(uart0, note); //Must be 0 - 127
+            uart_putc(uart0, data); //Must be 0 - 127
+}
+void target_off(uint8_t channel, uint8_t note, u_int8_t data){
+            uart_putc(uart0, 0x80 | channel); //Must be 0 - 15
+            uart_putc(uart0, note); //Must be 0 - 127
+            uart_putc(uart0, data); //Must be 0 - 127
+}
+
+int main() {
+    stdio_init_all();
+    gpio_init(TEST_LED_0);
+    gpio_set_dir(TEST_LED_0, true);
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, true);
+
+    //Button inputs
+    gpio_init(SW_1);
+    gpio_pull_up(SW_1);
+    gpio_set_dir(SW_1, false);
+    gpio_init(SW_2);
+    gpio_pull_up(SW_2);
+    gpio_set_dir(SW_2, false);
+    gpio_init(SW_3);
+    gpio_pull_up(SW_3);
+    gpio_set_dir(SW_3, false);
+
+    // Initialize RS232
+    uart_init(uart0, BAUD_RATE);
+    gpio_set_function(UART0_TX, GPIO_FUNC_UART);
+    gpio_set_function(UART0_RX, GPIO_FUNC_UART);   
+
+    //Seven segmet outputs
+    gpio_init(SS_LAT);
+    gpio_set_dir(SS_LAT, true);
+    gpio_init(SS_BLANK);
+    gpio_set_dir(SS_BLANK, true);
+
+    //Set Brightness
+    uint8_t ss_currents[] = {254, 238, 0, 255, 255}; //Use this to fine tune brightness of the displays end of chain first
+    set_tlc5916_current(ss_currents, DISPLAY_UNITS);
+
+    // Initialize SPI
+    spi_init(SS_SPI, SS_SPI_RATE); // Initialize SPI
+    gpio_set_function(SS_D, GPIO_FUNC_SPI);
+    gpio_set_function(SS_CLK, GPIO_FUNC_SPI);
+    spi_set_format(SS_SPI, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+    spi_set_slave(SS_SPI, false); // We're the master
+    int count = 0;
+
+    // Initialize hardware timer
+    struct repeating_timer timer;
+
+    gpio_put(SS_BLANK, 0);
+    //boot_text();//display boot message UCOMMENT ME LATER
+    
+    // Initialize the timer with the given period and enable interrupts
+    add_repeating_timer_ms(-100, timer_callback, NULL, &timer);
+
+    // Set up interrupt for falling edge on button pin
+    //gpio_set_irq_enabled_with_callback(SW_1, GPIO_IRQ_EDGE_RISE, true, &button_callback);
+
+    while(true){
+        gpio_put(LED_PIN, gpio_get(SW_1));
+        gpio_put(TEST_LED_0, 1);
+        if(!gpio_get(SW_1)){
+            game_time = 1800;
+            target_on(1, 0, 0);
+            target_on(1, 1, 1);
+            target_on(1, 2, 2);
+            target_on(1, 3, 3);
+        }
+
+        score[0]++;
+        score[1]+=3;
+        score[2]+=5;
+        sleep_ms(300);
+    }
+    
+}
--- software/small target/source.c
+++ software/small target/source.c
@@ -8,6 +8,7 @@
 #include "hardware/dma.h"
 #include "hardware/irq.h"
 #include "hardware/spi.h"
+#include "hardware/uart.h"
 #include "ws2812.pio.h"
 
 #define LED_PIN PICO_DEFAULT_LED_PIN
@@ -21,25 +22,48 @@
 
 #define LED_0 10
 
+#define BAUD_RATE 31250 //MIDI standard data rate
 #define UART0_TX 0
 #define UART0_RX 1
 
-#define LED_COUNT 70
-#define STRIP_COUNT 2
+#define PLAYER0_COLOR 0xFF000000
+#define PLAYER1_COLOR 0x00FF0000
+#define PLAYER2_COLOR 0x0000FF00
+#define PLAYER3_COLOR 0x60900000
+
+#define LEDS_PER_TARGET 35
+#define TARGETS_PER_STRING 2
+#define LED_COUNT (LEDS_PER_TARGET*TARGETS_PER_STRING)
+#define STRING_COUNT 2
+
+#define UART_MSG_SIZE 3
+#define BUFFER_CAPACITY 16 
 
 bool update_flag = false;
-uint dma_chan[STRIP_COUNT];
-uint32_t led_colors[STRIP_COUNT][LED_COUNT];
+uint dma_chan[STRING_COUNT];
+uint32_t led_colors[STRING_COUNT][LED_COUNT] = {0};
+uint32_t current_pat[TARGETS_PER_STRING*STRING_COUNT];
+volatile uint32_t time_click = 0;
+uint32_t fade[35] = {
+    0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 1, 
+    2, 4, 8, 16, 32, 
+    48, 64, 80, 96, 112,
+    128, 144, 160, 176, 192,
+    208, 224, 240, 256, 256
+};
 
 bool timer_callback (struct repeating_timer *t) {
-    for (int i = 0; i < STRIP_COUNT; i++)
+    time_click++;
+    for (int i = 0; i < STRING_COUNT; i++)
     {
         if (dma_channel_is_busy(dma_chan[i])) {
             return false;
         }
     }
     
-    for (int i = 0; i < STRIP_COUNT; i++) {
+    for (int i = 0; i < STRING_COUNT; i++) {
         dma_channel_set_read_addr(dma_chan[i], led_colors[i], false );
         dma_channel_set_trans_count(dma_chan[i], LED_COUNT, false);
         dma_channel_start(dma_chan[i]);
@@ -47,6 +71,54 @@ bool timer_callback (struct repeating_timer *t) {
     update_flag = true;
     return true;
 }
+
+uint32_t set_brightness(u_int32_t color, uint32_t brightness){//Unity brightness is 256 colors is 0xGGRRBBWW
+    u_int32_t green = ((color >> 24) & 0xFF) * brightness;
+    u_int32_t red = ((color >> 16) & 0xFF) * brightness;
+    u_int32_t blue = ((color >> 8) & 0xFF) * brightness;
+    color =((green << 16) & 0xFF000000)+((red << 8) & 0x00FF0000)+((blue) & 0x0000FF00);
+    return color;
+}
+
+void pattern_solid (uint32_t *data, uint32_t t, uint32_t color){
+    for (int i = 0; i < LEDS_PER_TARGET; i++)
+    {
+        data[i]= color;
+    }
+}
+
+void pattern_rotate (uint32_t *data, uint32_t t, uint32_t color){
+    for (int i = 0; i < LEDS_PER_TARGET; i++)
+    {
+        data[i] = set_brightness(color, fade[(t + i) % LEDS_PER_TARGET]);
+    }
+}
+
+void pattern_blamo (uint32_t *data, uint32_t t, uint32_t color){ //needs more blam
+    for (int i = 0; i < LEDS_PER_TARGET; i++)
+    {
+        data[i] = set_brightness(color, fade[(t) % LEDS_PER_TARGET]);
+    }
+}
+
+void pattern_off (uint32_t *data, uint32_t t, uint32_t color){
+    for (int i = 0; i < LEDS_PER_TARGET; i++)
+    {
+        data[i]= 0;
+    }
+}
+
+typedef void (*pattern)(uint32_t *data, uint32_t t, uint32_t color);
+const struct {
+    pattern pat;
+    const char *name;
+} pattern_table[] = {
+        {pattern_solid,  "Solid"},
+        {pattern_rotate,  "Rotate"},
+        {pattern_blamo,  "Blamo!"},
+        {pattern_off,  "Off"},
+};
+
 
 void setup_dma(PIO pio, uint sm, uint dma_chan, const uint32_t *source_addr) {
     dma_channel_config c = dma_channel_get_default_config(dma_chan);
@@ -58,11 +130,53 @@ void setup_dma(PIO pio, uint sm, uint dma_chan, const uint32_t *source_addr) {
 }
 
 void setup_leds() {
-    for (int i = 0; i < STRIP_COUNT; i++) {
+    for (int i = 0; i < STRING_COUNT; i++) {
         for (int j = 0; j < LED_COUNT; j++) {
-            led_colors[i][j] = 0x0F0000; // RBG Color
+            led_colors[i][j] = 0x000000; // RBG Color
         }
     }
+}
+
+uint8_t ring_buffer[BUFFER_CAPACITY][UART_MSG_SIZE];
+volatile int write_index = 0;
+volatile int read_index = 0;
+volatile int buffer_count = 0;
+
+// Function to handle UART interrupts. Currently hardcoded to uart0!
+void on_uart_rx() {
+    static uint8_t uart_data_buffer[UART_MSG_SIZE];
+    static int uart_data_index = 0;
+
+    while (uart_is_readable(uart0)) {
+        uint8_t byte = uart_getc(uart0);
+
+        // MIDI messages start with a status byte (0x80 to 0xFF)
+        if (byte & 0x80) {
+            uart_data_index = 0;  // Reset buffer index if we get a new status byte
+        }
+
+        uart_data_buffer[uart_data_index++] = byte;
+
+        // Check if we've received a complete MIDI message
+        if (uart_data_index == UART_MSG_SIZE) {
+            // Store the message in the ring buffer if there is space
+            if (buffer_count < BUFFER_CAPACITY) {
+                for (int i = 0; i < UART_MSG_SIZE; i++) {
+                    ring_buffer[write_index][i] = uart_data_buffer[i];
+                }
+                write_index = (write_index + 1) % BUFFER_CAPACITY;
+                buffer_count++;
+            }
+            uart_data_index = 0;  // Reset buffer index for next message
+        }
+    }
+}
+
+void process_uart_data(uint8_t *data) {
+    uint8_t command = data[0];
+    uint8_t data1 = data[1];
+    uint8_t data2 = data[2];
+    printf("MIDI Message: 0x%02X 0x%02X 0x%02X\n", command, data1, data2);
 }
 
 int main() {
@@ -74,46 +188,48 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, true);
 
+    //Initalize UARTs
+    uart_init(uart0, BAUD_RATE);
+    gpio_set_function(UART0_TX, GPIO_FUNC_UART);
+    gpio_set_function(UART0_RX, GPIO_FUNC_UART);
+
+    //Enable UART interrupt
+    uart_set_irq_enables(uart0, true, false);
+    irq_set_exclusive_handler(UART0_IRQ, on_uart_rx);
+    irq_set_enabled(UART0_IRQ, true);
+
     // Initialize hardware timer
     struct repeating_timer timer;
 
     PIO pio = pio0;
     uint offset = pio_add_program(pio, &ws2812_program);
-    setup_leds();
+    //setup_leds();
 
-    for (int i = 0; i < STRIP_COUNT; i++) {
+    for (int i = 0; i < STRING_COUNT; i++) {
         uint sm = pio_claim_unused_sm(pio, true);
         ws2812_program_init(pio, sm, offset, i + LED_0, 800000, false); // Assuming GPIO pins 0-3 for the 4 strips
         dma_chan[i] = dma_claim_unused_channel(true);
         setup_dma(pio, sm, dma_chan[i], led_colors[i]);
         dma_channel_start(dma_chan[i]);
     }
-    sleep_ms(500);
     // Initialize the timer with the given period and enable interrupts
-    add_repeating_timer_ms(-20, timer_callback, NULL, &timer); //50 fps
+    add_repeating_timer_ms(-20, timer_callback, NULL, &timer); //-20 for 50 fps
 
     while(true){
         gpio_put(TEST_LED_0, 1);
         gpio_put(TEST_LED_1, update_flag);
         gpio_put(LED_PIN, 1);
+        if (buffer_count > 0) {
+            process_uart_data(ring_buffer[read_index]);
+            read_index = (read_index + 1) % BUFFER_CAPACITY;
+            buffer_count--;
+        }
         if (update_flag) {
-            // for (int i = 0; i < STRIP_COUNT; i++) {
-            //     for (int j = 0; j < LED_COUNT; j++) {
-            //         led_colors[i][j] = 0x03030300; // RGB Color
-            //     }
-            // }
-            for (int j = 0; j < LED_COUNT; j++) {
-                led_colors[0][j] = 0x03030000; // RGB Color
-            }
-            for (int j = 0; j < LED_COUNT; j++) {
-                //led_colors[1][j] = 0x00030300; // RGB Color
-                led_colors[1][j] = (j)<<8; // RGB Color
-            }
-            led_colors[1][0] = 0x000F0000; //First LED RED
-            led_colors[1][LED_COUNT-1] = 0x0F000000; //Last LED green
+            pattern_rotate(&led_colors[0][0], time_click, PLAYER0_COLOR);
+            pattern_rotate(&led_colors[0][35], 0, PLAYER1_COLOR);
+            pattern_rotate(&led_colors[1][0], time_click, PLAYER2_COLOR);
+            pattern_rotate(&led_colors[1][35], 0, PLAYER3_COLOR);
         update_flag = false;
         }
-        printf("I AM RUNNING!! \n");
-        sleep_ms(200);
     }
 }
